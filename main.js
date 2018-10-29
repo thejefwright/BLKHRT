040f7e049ff22992173e797917fe85894744a629
jef wright <thejefwright@jefs-MacBook-Pro.local>
2018-10-29 08:06:50 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+      window.open("contact.html") ;
+}) 
+
+const nav = document.querySelector('.main');
+  const topOfNav = nav.offsetTop;
+
+  function fixNav() {
+    if(window.scrollY >= topOfNav){
+      document.body.style.paddingTop = nav.offsetHeight + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove('fixed-nav');
+    }
+  }
+
+  window.addEventListener('scroll', fixNav);
